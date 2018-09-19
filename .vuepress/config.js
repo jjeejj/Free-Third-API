7264bb3bcb7b690ecccfd76ac160c69031d4ba68
jjeejj <wenjunjiang1993@163.com>
2018-09-19 15:27:40 +0800

    updatePopup: true

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -10,7 +10,9 @@ module.exports = {
     themeConfig: {
         lastUpdated: 'Last Updated',
         searchMacxSuggestions: 50,
-        serviceWorker: true,
+        serviceWorker: {
+            updatePopup: true
+        },
         // displayAllHeaders: true,
         activeHeaderLinks: true,
         sidebarDepth: 2,
