98d01d9181e84b611ae31c8b0b39715a1483f5d8
jjeejj <wenjunjiang1993@163.com>
2018-09-19 15:14:02 +0800

    docs update serviceWorker

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -11,7 +11,7 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.2.0/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.4.1/workbox-sw.js");
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -21,60 +21,80 @@ importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.2.0/workbox
 self.__precacheManifest = [
   {
     "url": "404.html",
-    "revision": "3d3c570148be55f39aa00e8f0536559d"
+    "revision": "a1d37f807469c1fce21eabc5954ff5f7"
   },
   {
-    "url": "assets/css/5.styles.72743211.css",
-    "revision": "b312b8879b1f3d427e25b3ac081b371e"
+    "url": "assets/css/0.styles.39d8b704.css",
+    "revision": "0b104e06bbcdf55ac04c0380c87c0e0f"
   },
   {
     "url": "assets/img/search.83621669.svg",
     "revision": "83621669651b9a3d4bf64d1a670ad856"
   },
   {
-    "url": "assets/js/0.7929b963.js",
-    "revision": "9baada6ed1a5aa09f67d919d2fcb227d"
+    "url": "assets/js/2.285762ea.js",
+    "revision": "93afbd4e91ed17280e84a6930dc81b17"
   },
   {
-    "url": "assets/js/1.64380550.js",
-    "revision": "3f03391d4b26b035bf720de6dc5804af"
+    "url": "assets/js/3.eeb1c1bf.js",
+    "revision": "d4d31fdbf647530bf2534bc8ffed240f"
   },
   {
-    "url": "assets/js/2.10f081bf.js",
-    "revision": "16c10adc84abb3615ac20a650174fe80"
+    "url": "assets/js/4.f25f0bf1.js",
+    "revision": "00b8d03fd93f1fd4577b0fdc9e175824"
   },
   {
-    "url": "assets/js/3.83cb1d4d.js",
-    "revision": "09a5ffa7af068d70984d77c3eb19d178"
+    "url": "assets/js/5.7021a73e.js",
+    "revision": "69719d336e29e813891d94fc01b6f8b2"
   },
   {
-    "url": "assets/js/4.36e65d6f.js",
-    "revision": "efcc53750a384a55d6371c6e35086186"
+    "url": "assets/js/6.192db71b.js",
+    "revision": "c733cfeeb0c1ec8933399e081293d162"
   },
   {
-    "url": "assets/js/app.fc96dd7b.js",
-    "revision": "8439a2ef2c47496b5b135f44b89025e7"
+    "url": "assets/js/7.3b6d76de.js",
+    "revision": "78bc9daa904f8585b4c2ded7681865ac"
+  },
+  {
+    "url": "assets/js/8.01f671a9.js",
+    "revision": "0046424d97a1662f00f02c93cafd3e1b"
+  },
+  {
+    "url": "assets/js/app.303d9d54.js",
+    "revision": "9942ffcf8527a76683c4a046e8b905d3"
   },
   {
     "url": "googleTrends/index.html",
-    "revision": "76d0f96e97b942a2aec4f4d1f8f5dee8"
+    "revision": "cee893dbd2b31d23eb1c7476d2c65ec2"
   },
   {
     "url": "guide/index.html",
-    "revision": "9fb74c7f79f44df15052660dd16db4fa"
+    "revision": "4feb51e21d53e76205b6d04b90006eae"
   },
   {
     "url": "index.html",
-    "revision": "7d3ded36af069f7edc1536664fdb2f77"
+    "revision": "228c1bbdbd111b51922d2d4ef8afc1bd"
   },
   {
     "url": "tool/index.html",
-    "revision": "26d8953c07d240fbc4fc55597d415682"
+    "revision": "e648c28ebbcd2afb0d4fc2ef7e7242b7"
   },
   {
     "url": "动图宇宙/index.html",
-    "revision": "ff160944b034be9dad7ad46be818c336"
+    "revision": "fb8f3acfbebac14d97bc4dd0c18b3b0b"
   }
 ].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+addEventListener('message', event => {
+  const replyPort = event.ports[0]
+  const message = event.data
+  if (replyPort && message && message.type === 'skip-waiting') {
+    event.waitUntil(
+      self.skipWaiting().then(
+        () => replyPort.postMessage({ error: null }),
+        error => replyPort.postMessage({ error })
+      )
+    )
+  }
+})
